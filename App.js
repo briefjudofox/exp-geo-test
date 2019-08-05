542b74d44cd5187aba87465b4664fc87357beed8
briefjudofox <briefjudofox@users.noreply.github.com>
2019-08-05 16:38:45 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View ,FlatList, Button} from 'react-native';
+import GeofenceTask, {getEventHistory, clearEventHistory} from "./GeofenceTask"
+import * as Permissions from 'expo-permissions';
+
+Permissions.askAsync(Permissions.LOCATION)
+GeofenceTask.startAsync();
+
+export default function App() {
+
+
+  const [eventHistory, setEventHistory] = useState([]);
+
+  async function getGeofenceHistory() {
+    console.log("I AM CALLED HHHHHHH")
+    const geofenceEvents = await getEventHistory();
+    console.log(geofenceEvents)
+    setEventHistory(geofenceEvents);
+  }
+
+  async function clearGeofenceHistory() {
+    await clearEventHistory();
+    setEventHistory([]);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>{`Event History length ${eventHistory.length}`}</Text>
+      <FlatList
+        data={eventHistory}
+        renderItem={({item}) => <Text>{`${item.key} | ${item.value.eventType === 1 ? "ENTER" : "EXIT"} | ${item.value.region}`}</Text>}
+      />
+      <View style={{flexDirection: "row"}}>
+        <Button
+          onPress={getGeofenceHistory}
+          title="Refresh History"
+          color="#FF0000"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Button
+          onPress={clearGeofenceHistory}
+          title="Clear History"
+          color="#0000FF"
+        />
+      </View>
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+    margin: 30,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
